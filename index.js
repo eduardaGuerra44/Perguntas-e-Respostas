fccf9845268e2a14f8fe31fd695998c8646ed4fb
Eduarda Guerra <eduardaguerra44@gmail.com>
2023-02-07 21:37:59 -0300

    registrando respostas no db e exibindo na para o usuario

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Pergunta = require('./models/Pergunta');
@@ -7,8 +7,8 @@ const Resposta = require('./models/Resposta');
 //CONECTANDO O DB 
 const connection = require('./database/database');
 connection.authenticate()
-.then(()=>{ console.log("conexão feita com o db")})
-.catch((msgerro)=>{console.log(msgerro)})
+    .then(() => { console.log("conexão feita com o db") })
+    .catch((msgerro) => { console.log(msgerro) })
 
 //configirando ejs 
 app.set('view engine', 'ejs')
@@ -16,23 +16,25 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 // //bodyParser
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(bodyParser.json)
 
 // rotas 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     //pesquisando perguntas e armazenando em uma variável. jogando para a pág home
-    Pergunta.findAll({raw: true, order:[
-        ['id','desc']]}).then(perguntas => 
-            {res.render('index.ejs', 
+    Pergunta.findAll({
+        raw: true, order: [
+            ['id', 'desc']]
+    }).then(perguntas => {
+        res.render('index.ejs',
             {
-            perguntas: perguntas
-        })
+                perguntas: perguntas
+            })
     })
 });
 
 
-app.get('/perguntar', (req, res)=> res.render('perguntar.ejs'))
+app.get('/perguntar', (req, res) => res.render('perguntar.ejs'))
 
 //rota que recebe infos do formulário
 app.post("/salvarpergunta", (req, res) => {
@@ -43,27 +45,45 @@ app.post("/salvarpergunta", (req, res) => {
         titulo: titulo,
         descricao: descricao
     })
-    // se salva com sucesso redireciona para a home
-    .then(()=>{ 
-        res.redirect('/')
-    })
+        // se salva com sucesso redireciona para a home
+        .then(() => {
+            res.redirect('/')
+        })
 });
 
 //acessando pergunta por id
-app.get('/pergunta/:id', (req, res) =>{
+app.get('/pergunta/:id', (req, res) => {
     var id = req.params.id;
     Pergunta.findOne({
-        where: {id: id}
+        where: { id: id }
     }).then(pergunta => {
-        if(pergunta != undefined){
-            res.render('pergunta.ejs',{
-                pergunta:pergunta
+        if (pergunta != undefined) {
+
+            Resposta.findAll({
+                where: { perguntaId: pergunta.id },
+                order:[['id', 'DESC']]
+            }).then(respostas => {
+                res.render('pergunta.ejs', {
+                    pergunta: pergunta,
+                    respostas: respostas
+                })
             })
-        }else{
+        } else {
             res.redirect('/')
         }
     })
 });
-    
 
-app.listen(3000, ()=> console.log('app rodando'))
+app.post('/responder', (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect('/pergunta/' + perguntaId)
+    })
+})
+
+
+app.listen(3000, () => console.log('app rodando'))
